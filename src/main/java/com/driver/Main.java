e9c087f31d4a1f9bac30fbaca274bfb27b61144d
abhinavsrivastava <abhinavsrivastav687@gmail.com>
2023-05-14 19:24:45 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class  Product {
+        public  int Product(int x, int y) {
+            return x * y;
+        }
 
+        public  int Product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public   double Product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public  static void Main (String args[]) {
+        Product p = new Product () ;
+        System.out.println(p.Product(5, 6));
+        System.out.println(p.Product(4, 5, 9));
+        System.out.println(p.Product(2.6, 5.3));
+    }
 }
