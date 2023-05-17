5dcbc611e732c4f55f5d7b564f7903ae479be478
abhinavsrivastava <abhinavsrivastav687@gmail.com>
2023-05-17 12:53:17 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,16 @@
 package com.driver;
 
 public class Main {
-    static class  Product {
-        public  int Product(int x, int y) {
-            return x * y;
-        }
-
-        public  int Product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public   double Product(double x, double y) {
-            return x * y;
-        }
+     static class  Product {
+      public int Product (int x , int y){
+          return x * y ;
+      }
+      public int Product (int x , int y , int z){
+          return x * y *z ;
+      }
+      public double Product (double x , double y){
+          return x * y ;
+      }
     }
 
     public  static void Main (String args[]) {
